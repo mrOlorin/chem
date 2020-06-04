168990367918730bb89823c326cd14015742aa62
mr.olorin <mr.olorin@gmail.com>
2020-06-04 18:51:57 +0300

    Remove list components;

--- src/views/ElementList.vue
+++ src/views/ElementList.vue
@@ -1,17 +1,35 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Action, State } from 'vuex-class';
+import Particles from '@/store/Particles';
 
 @Component({
   name: 'element-list',
   components: {
-    ElementList: () => import('@/components/ElementList.vue')
+    ElementInfo: () => import('@/components/ElementInfo.vue')
   }
 })
 export default class ElementList extends Vue {
+  @State('Particles')
+  public state!: Particles;
+
+  @Action
+  public buildAtoms!: () => void;
+
+  private beforeMount () {
+    this.buildAtoms();
+  }
 };
 </script>
+<style>
+    .element {
+        display: inline-block;
+    }
+</style>
 <template>
     <div class="element-list">
-        <ElementList/>
+        <div class="element" v-for="(element, i) in state.atoms" v-bind:key="i">
+            <ElementInfo :atom="element" :id="`element-${element.nucleus.Z}`"/>
+        </div>
     </div>
 </template>
--- src/views/NuclideList.vue
+++ src/views/NuclideList.vue
@@ -1,17 +1,52 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Action, State } from 'vuex-class';
+import Particles from '@/store/Particles';
+import ISOTOPES from '@/chem/literals/isotopes';
 
 @Component({
   name: 'nuclide-list',
   components: {
-    NuclideList: () => import('@/components/NuclideList.vue')
+    NuclideInfo: () => import('@/components/NuclideInfo.vue')
   }
 })
 export default class NuclideList extends Vue {
+  @State('Particles')
+  public particles!: Particles;
+
+  @Action
+  public buildNuclides!: () => void;
+
+  protected ISOTOPES: Array<[number, number, number]> = ISOTOPES;
+
+  private beforeMount () {
+    this.buildNuclides();
+  }
+
+  public mounted () {
+    this.$el.addEventListener('wheel', this.onWheel);
+  }
+
+  private beforeDestroy () {
+    this.$el.removeEventListener('wheel', this.onWheel);
+  }
+
+  private onWheel (e: Event) {
+    const f = this.$el.scrollWidth / (this.$el.scrollHeight);
+    e.preventDefault();
+    window.scrollBy((e as WheelEvent).deltaY * f, (e as WheelEvent).deltaY);
+  }
 };
 </script>
 <template>
     <div class="nuclide-list">
-        <NuclideList/>
+        <table>
+            <tr v-for="(isotopes, i) in particles.nuclides" v-bind:key="i">
+                <td :colspan="ISOTOPES[i][0] + 1"></td>
+                <td v-for="(nuclide, j) in isotopes" v-bind:key="j">
+                    <NuclideInfo v-if="nuclide" v-bind:nuclide="nuclide"/>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
